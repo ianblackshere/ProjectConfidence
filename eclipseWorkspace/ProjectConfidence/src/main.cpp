cc15eb56202814a558ff34c25e75b1d5dbecba75
Ian Maloney <ianBmaloney@Ians-MBP.lan>
2020-07-12 09:00:33 -0700

    Created eclipse workspace with main file.

---
+++ eclipseWorkspace/ProjectConfidence/src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ ============================================================================
+ Name        : ProjectConfidence.cpp
+ Author      : ianblackshere
+ Version     : 2020.1
+ Copyright   : None.
+ Description : Main file and executive entry point for the executable.
+ ============================================================================
+ */
+
+
+void main(void)
+{
+	return;
+}
